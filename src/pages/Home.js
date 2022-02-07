a824ab5d533b47684656ef7d592577ac37d54351
jayendra <jaimahakalb4m@gmail.com>
2022-02-08 00:20:30 +0530

    added moe platelet revised css a bit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,24 +2,28 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import { Navigate } from "react-router-dom";
 import { useGlobalContext } from "../context";
+import moeimg from "../img/platelet.png";
 function Home() {
   const { user } = useGlobalContext();
   return (
     <>
       {user && <Navigate to="/dashboard" />}
       <Wrapper className="page">
-        <div className="info">
+        <div className="cover-card">
+          <img src={moeimg} alt="" className="moe" />
           <p>
             I'm baby viral enamel pin chartreuse cliche retro af selfies kinfolk
             photo booth plaid jianbing actually squid 3 wolf moon lumbersexual.
             Hell of humblebrag gluten-free lo-fi man braid leggings.
           </p>
-          <Link to="/login" className="btn">
-            Login
-          </Link>
-          <Link to="/register" className="btn">
-            Register
-          </Link>
+          <div className="btn-container">
+            <Link to="/login" className="btn">
+              Login
+            </Link>
+            <Link to="/register" className="btn">
+              Register
+            </Link>
+          </div>
         </div>
       </Wrapper>
     </>
@@ -27,27 +31,31 @@ function Home() {
 }
 
 const Wrapper = styled.div`
-  display: grid;
-  align-items: center;
-  h2 {
-    font-weight: 700;
+  .cover-card {
+    box-sizing: inherit;
+    font-family: "Roboto", "Open Sans", sans-serif;
+    border: 0;
+    background-color: #0000001f;
+    color: white;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -30%);
+    width: 70%;
+    height: 70%;
+    max-width: 600px;
+    max-height: 600px;
+    padding: 1.25rem;
+    text-align: center;
   }
-  h2 span {
-    color: var(--primary-500);
+  .moe {
+    position: absolute;
+    transform: translate(100%, -85%);
+    width: 150px;
+    height: 200px;
+    z-index: 2;
   }
-  .main-img {
-    display: none;
-  }
-  @media (min-width: 992px) {
-    grid-template-columns: 1fr 1fr;
-    column-gap: 6rem;
-    .main-img {
-      display: block;
-    }
-  }
-  .btn {
-    margin-left: 0.25rem;
-    margin-right: 0.25rem;
+  .btn-container {
   }
 `;
 
